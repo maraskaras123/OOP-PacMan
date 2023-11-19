5a836d614d3076dfdd66cce05739da973c037f98
Arnas Marozas <maraskaras123@gmail.com>
2023-11-19 17:49:22 +0200

    prep for deploys

--- PacMan/PacMan/Client/Program.cs
+++ PacMan/PacMan/Client/Program.cs
@@ -8,13 +8,19 @@ namespace PacMan.Client
 {
     public class Program
     {
+        public static readonly string ApiUrl = "https://pacman-api.arnasm.dev";
+
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient
+            {
+                BaseAddress =
+                    new(builder.HostEnvironment.IsDevelopment() ? builder.HostEnvironment.BaseAddress : ApiUrl)
+            });
             builder.Services.AddSingleton<IInputHandler, KeyboardInputHandler>();
             builder.Services.AddSingleton<InputService>();
             builder.Services.AddScoped<StartCommand>();
